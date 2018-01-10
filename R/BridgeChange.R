045df40d3c92668e49f0e052f651d2afc239a9c2
sou412 <sou412snow@gmail.com>
2018-01-10 16:33:04 +0900

    minor fix

--- R/BridgeChange.R
+++ R/BridgeChange.R
@@ -10,10 +10,12 @@ NULL
 #'
 #' This package provides Bayesian implementation of Bridge regression with possible change-points.
 #' The package supports univariate time-series data as well as panel data with linear and non-linear link functions.
-#' In addition to Gaussian linear models, count and binary outcomes are supported.
+#' In addition to the Gaussian linear models, count and binary outcomes are supported.
 #'
 #' Specifically, this package offers the following functions:
 #' \itemize{
-#'   \item \code{\link{BridgeChangeReg}} Univariate Regression with Change point.
+#'   \item \code{\link{BridgeChangeReg}}: Univariate Regression with Change-points.
+#'   \item \code{\link{BridgeMixedPanel}}: Mixed Effect Panel Regression with Change-points.
+#'   \item \code{\link{BridgeFixedPanel}}: Fixed Effect Panel Regression with Change-points.
 #' }
 "_PACKAGE"
