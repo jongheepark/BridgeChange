c89d5ee2f221ba1df5e702aa943a769bec1d1c3d
sou412 <sou412snow@gmail.com>
2021-02-28 15:44:30 -0500

    Remvoe duplicate functiion

--- R/BridgeChangeFixedPanelHybrid.R
+++ R/BridgeChangeFixedPanelHybrid.R
@@ -14,10 +14,10 @@
 #' @param model Model (\code{c("within","between", "pooling")}).
 #' @param effect Effect (\code{c("individual", "time", "twoways")}).
 #' @param standardize If TRUE, all covariates are standardized.
-#' @param interaction If TRUE, all pairwise interactions are added. 
+#' @param interaction If TRUE, all pairwise interactions are added.
 #' @param n.break Number of breaks.
 #' If \code{n.break = 0}, it simply runs fixed effect model with shrinkage prior on coefficients.
-#' @param ols.weight If TRUE, OLS estimates are used for adpative lasso weight vector. 
+#' @param ols.weight If TRUE, OLS estimates are used for adpative lasso weight vector.
 #' @param mcmc MCMC iteration.
 #' @param burn Burn-in period.
 #' @param verbose Verbose.
@@ -34,10 +34,10 @@
 #'
 #' @export
 #'
-#' 
+#'
 ## Adaptive for each regime
 BridgeFixedPanelHybrid <- function(formula, data, index, model, effect,
-                             standardize = TRUE, interaction = FALSE, 
+                             standardize = TRUE, interaction = FALSE,
                              n.break = 1, ols.weight = FALSE, sparse.only = FALSE,
                              mcmc = 100, burn = 100, verbose = 100, thin = 1,
                              b0=0, B0=1, c0 = 0.1, d0 = 0.1, r0 =  1, R0 = 1,
@@ -53,7 +53,7 @@ BridgeFixedPanelHybrid <- function(formula, data, index, model, effect,
     #     dat.sd <- apply(data[, !(colnames(data) %in% index)], 2, sd)
     #     data <- data.frame(cbind(scale(data[, !(colnames(data) %in% index)]), data[,index]))
     # }
-    
+
     pdata    <- pdata.frame(data, index)
     pformula <- pFormula(formula)
 
@@ -62,10 +62,10 @@ BridgeFixedPanelHybrid <- function(formula, data, index, model, effect,
 
     plmX <- X
     plmy <- y
-    
-    m <- n.break 
+
+    m <- n.break
     W <- matrix(0, length(y), 1)
-    
+
     if(interaction){
         x1.1 <- data.frame(X)
         var.names <- colnames(X)
@@ -77,14 +77,14 @@ BridgeFixedPanelHybrid <- function(formula, data, index, model, effect,
     unscaled.Y <- y
     unscaled.X <- X
 
-    if (standardize) { 
+    if (standardize) {
         ysd <- sd(y)
         Xsd <- apply(X, 2, sd)
-        ## if Xsd is exactly 0, use 1. 
+        ## if Xsd is exactly 0, use 1.
         ## Xsd <- ifelse(Xsd == 0, 1, Xsd)
         dat.sd <- c(ysd, Xsd)
         X <- scale(X)
-        y <- scale(as.vector(y))        
+        y <- scale(as.vector(y))
     }
 
 
@@ -99,8 +99,8 @@ BridgeFixedPanelHybrid <- function(formula, data, index, model, effect,
     }else{
         output <- BridgeMixedPanel(subject.id = subject.id, time.id = time.id, y=as.vector(y), X=X, W=W,
                                    n.break = n.break, b0=b0, B0=B0, c0=c0, d0=d0, r0=r0, R0=R0,
-                                   standardize = standardize, alpha.MH=TRUE, 
-                                   mcmc = mcmc, burn = burn, thin = thin, verbose=verbose, 
+                                   standardize = standardize, alpha.MH=TRUE,
+                                   mcmc = mcmc, burn = burn, thin = thin, verbose=verbose,
                                    nu.shape = 2.0, nu.rate = 2.0, alpha = 1, Waic = Waic, marginal = marginal, fixed = TRUE,
                                    unscaled.Y = unscaled.Y, unscaled.X = unscaled.X)
     }
@@ -114,7 +114,7 @@ BridgeFixedPanelHybrid <- function(formula, data, index, model, effect,
         yhat.state <- X%*%beta.mean
         ## yhat.state <- yhat.state - mean(yhat.state)
         state.indicator <- state[time.id]
-        
+
         ## Following P. Richard HAHN and Carlos M. CARVALHO (Eq. 21)
         yhat <- sapply(1:length(y), function(tt){yhat.state[tt,state.indicator[tt]]})
         unique.time.index <- sort(unique(attr(plmX,"index")[,2]))
@@ -128,12 +128,12 @@ BridgeFixedPanelHybrid <- function(formula, data, index, model, effect,
             dat.y <- data.frame(yhat[ is.element(attr(plmX,"index")[,2], unique.time.index[state==i])])
             raw.y <- data.frame(y[ is.element(attr(plmX,"index")[,2], unique.time.index[state==i])])
             dat.id <- subject.id[is.element(attr(plmX,"index")[,2], unique.time.index[state==i])]
-            
-            ## group centering 
-            x.list[[i]] <- as.matrix(group.center(dat.x, dat.id))    
+
+            ## group centering
+            x.list[[i]] <- as.matrix(group.center(dat.x, dat.id))
             y.list[[i]] <- as.matrix(group.center(dat.y, dat.id))
-            raw.y.list[[i]] <- as.matrix(group.center(raw.y, dat.id))    
-            ## x.list.0[[i]] <- as.matrix(dat.x)    
+            raw.y.list[[i]] <- as.matrix(group.center(raw.y, dat.id))
+            ## x.list.0[[i]] <- as.matrix(dat.x)
             ## y.list.0[[i]] <- as.matrix(dat.y)
             ## raw.y.list.0[[i]] <- as.matrix(raw.y)
         }
@@ -143,7 +143,7 @@ BridgeFixedPanelHybrid <- function(formula, data, index, model, effect,
             hybrid.dss <- sapply(1:n.state, function(i){adaptive.lasso.olsweight(y.list[[i]], x.list[[i]])})
             rownames(hybrid.dss) <- colnames(X)
             colnames(hybrid.dss) <- paste0("Regime", 1:n.state)
-            
+
             hybrid.cp <- sapply(1:n.state, function(i){adaptive.lasso.olsweight(raw.y.list[[i]], x.list[[i]])})
             rownames(hybrid.cp) <- colnames(X)
             colnames(hybrid.cp) <- paste0("Regime", 1:n.state)
@@ -151,11 +151,11 @@ BridgeFixedPanelHybrid <- function(formula, data, index, model, effect,
             hybrid.dss <- sapply(1:n.state, function(i){adaptive.lasso(y.list[[i]], x.list[[i]], beta.hat = beta.mean[,i])})
             rownames(hybrid.dss) <- colnames(X)
             colnames(hybrid.dss) <- paste0("Regime", 1:n.state)
-            
+
             hybrid.cp <- sapply(1:n.state, function(i){adaptive.lasso(raw.y.list[[i]], x.list[[i]], beta.hat = beta.mean[,i])})
             rownames(hybrid.cp) <- colnames(X)
             colnames(hybrid.cp) <- paste0("Regime", 1:n.state)
-        }    
+        }
         ## hybrid.dss.0 <- sapply(1:n.state, function(i){adaptive.lasso(y.list.0[[i]], x.list.0[[i]])})
         ## rownames(hybrid.dss.0) <- colnames(X)
         ## colnames(hybrid.dss.0) <- paste0("Regime", 1:n.state)
@@ -177,7 +177,7 @@ BridgeFixedPanelHybrid <- function(formula, data, index, model, effect,
             ## cat("R-squared (DSS) : ", R2.DSS[i], "and R-squared (CP) ", R2.CP[i], "\n")
             cat("Pseudo R-squared (DSS) : ", R2.DSS.jhp[i], "and Pseudo R-squared (CP) ", R2.CP.jhp[i], "\n")
         }
-        ## summary of pseudo R-squared 
+        ## summary of pseudo R-squared
         yhat.dss <- X%*%hybrid.dss
         yhat.cp <- X%*%hybrid.cp
         yhat.sparse.dss <- sapply(1:length(y), function(tt){yhat.dss[tt, state.indicator[tt]]})
@@ -187,7 +187,7 @@ BridgeFixedPanelHybrid <- function(formula, data, index, model, effect,
 
         cat("Total :---------------------------------------------------------------------- \n Pseudo R-squared (DSS) : ", R2.DSS.jhp.total, "and Pseudo R-squared (CP) ", R2.CP.jhp.total, "\n")
 
-        
+
     }else{
         if(sparse.only){
             dat.id <- subject.id
@@ -205,23 +205,23 @@ BridgeFixedPanelHybrid <- function(formula, data, index, model, effect,
             ## yhat <- yhat - mean(yhat)
             dat.id <- subject.id
             ## cat("y and yhat correlation is ", cor(yhat, y), "\n")
-            
+
             ## group centering
             dat.x <- as.matrix(group.center(X, dat.id))
             dat.y <- as.matrix(group.center(matrix(yhat), dat.id))
-            
+
             ## Variable selection using adaptive lasso
             if(ols.weight){
                 hybrid.dss <- matrix(adaptive.lasso.olsweight(dat.y, dat.x), ncol(X), 1)
                 rownames(hybrid.dss) <- colnames(X)
-                
+
                 raw.y <- as.matrix(group.center(matrix(y), dat.id))
                 hybrid.cp <- matrix(adaptive.lasso.olsweight(raw.y, dat.x), ncol(X), 1)
                 rownames(hybrid.cp) <- colnames(X)
             }else{
                 hybrid.dss <- matrix(adaptive.lasso(dat.y, dat.x, beta.mean), ncol(X), 1)
                 rownames(hybrid.dss) <- colnames(X)
-                
+
                 raw.y <- as.matrix(group.center(matrix(y), dat.id))
                 hybrid.cp <- matrix(adaptive.lasso(raw.y, dat.x, beta.mean), ncol(X), 1)
                 rownames(hybrid.cp) <- colnames(X)
@@ -236,7 +236,7 @@ BridgeFixedPanelHybrid <- function(formula, data, index, model, effect,
             R2.CP.jhp <- r.square.sparse.jhp(y = raw.y, yhat = yhat, x = X,
                                              beta.sparse = hybrid.cp)
             cat("R-squared (DSS) : ", R2.DSS, "and R-squared (CP) ", R2.CP, "\n")
-            
+
         }
     }
     attr(output, "hybrid") <- hybrid.dss
@@ -289,101 +289,14 @@ BridgeFixedPanelHybrid <- function(formula, data, index, model, effect,
 #'
 #' @export
 #'
-#' 
+#'
 ## Adaptive for each regime
 adaptive.lasso <- function(y, x){
     fit.ridge <- cv.glmnet(y = y, x = x, type.measure="mse",
                            alpha=0, standardize = TRUE, family="gaussian")
     w3 <- 1/abs(matrix(coef(fit.ridge, s=fit.ridge$lambda.min)[, 1][2:(ncol(x)+1)] ))^1 ## Using gamma = 1
-    w3[w3[,1] == Inf] <- 999999999 ## Replacing values estimated as Infinite for 999999999   
+    w3[w3[,1] == Inf] <- 999999999 ## Replacing values estimated as Infinite for 999999999
     cv.adaptive <- cv.glmnet(x=x, y=y, family='gaussian', alpha=1, penalty.factor=w3)
     beta.adaptive <- coef(cv.adaptive)[-1]
     return(beta.adaptive)
-}
-
-
-BridgeFixedPanelHybrid <- function(formula, data, index, model, effect,
-                             standardize = TRUE, inter = FALSE, 
-                             n.break = 1, 
-                             mcmc = 100, burn = 100, verbose = 100, thin = 1,
-                             b0=0, B0=1, c0 = 0.1, d0 = 0.1, r0 =  1, R0 = 1,
-                             nu.shape = 2.0, nu.rate = 2.0, alpha = 1,
-                             Waic = FALSE, marginal = FALSE) {
-    call <- match.call()
-    a = NULL; b = NULL
-    ## ---------------------------------------------------- ##
-    ## use plm package here
-    ## transform data int pdata.frame object
-    ## ---------------------------------------------------- ##
-    # if (standardize) {
-    #     dat.sd <- apply(data[, !(colnames(data) %in% index)], 2, sd)
-    #     data <- data.frame(cbind(scale(data[, !(colnames(data) %in% index)]), data[,index]))
-    # }
-    
-    pdata    <- pdata.frame(data, index)
-    pformula <- pFormula(formula)
-
-    X <- plm:::model.matrix.pFormula(pformula, pdata, rhs = 1, model = model, effect = effect)
-    y <- plm:::pmodel.response(pformula, pdata, model = model, effect = effect)
-
-    plmX <- X
-    plmy <- y
-    
-    m <- n.break 
-    W <- matrix(0, length(y), 1)
-    
-    if(inter){
-        x1.1 <- data.frame(X)
-        var.names <- colnames(X)
-        x1.2 <- matrix(t(apply(x1.1, 1, combn, 2, prod)), nrow = nrow(X))
-        newX <- as.matrix(cbind(x1.1, x1.2))
-        colnames(newX) <- c(var.names, combn(var.names, 2, paste, collapse="-"))
-        X <- newX
-    }
-    unscaled.Y <- y
-    unscaled.X <- X
-
-    if (standardize) { 
-        ysd <- sd(y)
-        Xsd <- apply(X, 2, sd)
-        dat.sd <- c(ysd, Xsd)
-        X <- scale(X)
-        y <- scale(as.vector(y))        
-    }
-
-
-    subject.id <- as.numeric(as.factor(data[,index[1]]))
-    time.id    <- as.numeric(as.factor(data[,index[2]]))
-
-    ## ---------------------------------------------------- ##
-    ## run change point model on demeaned data
-    ## ---------------------------------------------------- ##
-    output <- BridgeMixedPanel(subject.id = subject.id, time.id = time.id, y=as.vector(y), X=X, W=W,
-                               n.break = n.break, b0=b0, B0=B0, c0=c0, d0=d0, r0=r0, R0=R0,
-                               standardize = FALSE,
-                               mcmc = mcmc, burn = burn, thin = thin, verbose=verbose, 
-                               nu.shape = 2.0, nu.rate = 2.0, alpha = 1, Waic = Waic, marginal = marginal, fixed = TRUE,
-                               unscaled.Y = unscaled.Y, unscaled.X = unscaled.X)
-
-    ## data preparation for regime-wise regression
-    state <- round(apply(attr(output, "s.store"), 2, mean))
-    unique.time.index <- sort(unique(attr(plmX,"index")[,2]))
-    n.state <- length(unique(state))
-    y.list <- x.list <- as.list(rep(NA, n.state))
-    for(i in 1:n.state){
-        x.list[[i]] <- X[ is.element(attr(plmX,"index")[,2], unique.time.index[state==i]), ]
-        y.list[[i]] <- y[ is.element(attr(plmX,"index")[,2], unique.time.index[state==i])]    
-    }
-    ## Variable selection using adaptive lasso
-    res.inter <- sapply(1:n.state, function(i){adaptive.lasso(y.list[[i]], x.list[[i]])})
-    rownames(res.inter) <- colnames(X)
-    colnames(res.inter) <- paste0("Regime", 1:n.state)
-    
-    attr(output, "hybrid") <- res.inter
-    attr(output, "title")  <- "SparseChangeFixedPanel Posterior Sample"
-    attr(output, "m")      <- n.break
-    if(standardize) attr(output, "dat.sd") <- dat.sd
-    # attr(output, "plm")     <- pm
-    # class(output) <- c("mcmc", "BridgeChange")
-    return(output)
 }
